e7667c0a17e406a6853fb64f7640072dd17f1a60
brandontaylor156 <brandon.david4@outlook.com>
2022-09-07 22:29:26 -0700

    product manager 1

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,25 @@
+const { Product } = require('../models/product.model');
+
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.findAllProducts = (req, res) => {
+    Product.find()
+      .then(allProducts => res.json({ products: allProducts }))
+      .catch(err => res.json({ message: "Something went wrong", error: err }));
+  };
+
+module.exports.createProduct = (request, response) => {
+    const { title, price, description } = request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => response.json(product))
+        .catch(err => response.json(err));
+}
+
